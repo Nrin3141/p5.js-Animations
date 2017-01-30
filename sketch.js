bdaa5a632bcd5c5c163d3fc3a1244e470462a5fa
Nrin <ricotrebeljahr@yahoo.de>
2017-01-30 15:01:07 +0100

    .

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 var button;
 var bgcolor;
 function setup(){
-  createCanvas(200, 200);
-  var button = createButton("Random Color");
+  canvas = createCanvas(200, 200);
+  bgcolor = color(200);
+  button = createButton("Random Color");
   button.mousePressed(changeColor);
 }
 function changeColor(){
-  bgcolor = random(40,50,60);
+  bgcolor = color(random(40,50,60));
 }
 function draw(){
   background(bgcolor);
