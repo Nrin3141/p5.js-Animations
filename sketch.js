4f2118d2b652220322f5aec9d3de2121a26750ec
Nrin <ricotrebeljahr@yahoo.de>
2017-01-31 23:54:40 +0100

    added buttons

--- sketch.js
+++ sketch.js
@@ -1,16 +1,29 @@
 var button;
+var button2;
+var button3;
 var bgcolor;
 
 function setup(){
   button = createButton("Delete");
   button.parent('button-position');
+  button2 = createButton("Stopp");
+  button2.parent('button-position');
+  button3 = createButton("Go On");
+  button3.parent('button-position');
   var canvas = createCanvas(1600, 750);
   bgcolor = color((242,242,242));
   background(bgcolor);
   canvas.parent('canvas-position');
   button.mousePressed(refresh);
+  button2.mousePressed(noLoop);
+  button3.mousePressed(loop);
 }
-
+function noLoop(){
+  noLoop();
+}
+function loop(){
+  loop();
+}
 function refresh(){
   noCanvas();
   canvas = createCanvas(1600, 750);
